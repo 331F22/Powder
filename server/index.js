3a2f6f91eb1924cbe769489e254c1c4483c8fd49
Daniel DeFrance <78085637+dldf@users.noreply.github.com>
2022-11-09 04:12:06 -0700

    Add files via upload
    Initial BSF-Starter code

---
+++ server/index.js
@@ -0,0 +1,79 @@
+const express = require('express')
+const bodyParser = require ('body-parser')
+const cors = require('cors')
+const app = express()
+const mysql = require('mysql')
+const dotenv = require('dotenv').config()
+
+const db = mysql.createPool({ // createConnection
+    host: 'localhost',
+    user: process.env.DBUSER,
+    password: process.env.DBPASS,
+    database: process.env.DATABASE,
+    port: process.env.DBPORT
+})
+
+app.use(cors())
+app.use(express.json())
+app.use(bodyParser.urlencoded({extended: true}))
+
+// READ
+app.get("/api/read", (req, res) => {
+    const sqlSelect = "SELECT * FROM volunteers;"
+    db.query(sqlSelect, (err, result) => { 
+        if(err){
+            throw err;
+        }
+        res.send(result);
+    })
+})
+
+// CREATE
+app.post("/api/create", (req, res) => {
+    const fn = req.body.first
+    const ln = req.body.last
+    const ea = req.body.email
+    const sqlInsert = "INSERT INTO volunteers (first_name, last_name, email_address) VALUES (?,?,?);"
+    db.query(sqlInsert, [fn, ln, ea], (err, result) => {
+        if(err) throw err
+        console.log("Server posted: ", fn, ln)
+        res.send(result)
+    })
+}) 
+
+// DELETE
+app.delete("/api/delete/:emailAddress", (req, res) => {
+    const ea = req.params.emailAddress;
+    console.log(ea)
+    const sqlDelete = "DELETE FROM volunteers WHERE email_address = ?";
+    db.query(sqlDelete, [ea], (err, result) => {
+        if(err) throw err
+        console.log("Server: deleted: ", ea)
+        res.send(result)
+    }) 
+})
+
+// UPDATE
+app.put("/api/update", (req, res) => {
+    // console.log(req)
+    
+    const ne = req.body.new;
+    const oe = req.body.old;
+    console.log("Ready to change: ", oe, "to", ne)
+    const sqlUpdate = "UPDATE volunteers SET email_address = ? WHERE email_address = ?"
+    db.query(sqlUpdate, [ne, oe], (err, result)=>{
+        if(err)  throw err;
+        console.log("Server changed: ", oe, "to", ne)
+        res.send(result)
+    })
+})
+
+const PORT = 3000 //process.env.EXPRESSPORT;
+const msg = `Running on PORT ${PORT}`
+app.get("/", (req, res) => {
+    res.send(`<h1>Express Server</h1><p>${msg}<p>`)
+})
+app.listen(PORT, () => {
+    console.log(msg)
+})
+
