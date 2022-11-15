387efb39a45a1b91f5de0e0954f2dffe3401f627
jjbrown23 <71121776+jjbrown23@users.noreply.github.com>
2022-11-14 19:47:26 -0700

    added semicolon

--- client/src/components/App/App.js
+++ client/src/components/App/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 import AddEntry from '../AddEntry.jsx';
 import CurrentEntries from '../CurrentEntries.jsx';
-import Footer from '../Footer.jsx'
+import Footer from '../Footer.jsx';
 
 function App() {
 
