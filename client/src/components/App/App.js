ec216f464b60ca9fc1964d0509636c3d5cdc4866
makaylabohnet <makayla.bohnet@gmail.com>
2022-11-14 17:35:39 -0700

    Footer Assignment

--- client/src/components/App/App.js
+++ client/src/components/App/App.js
@@ -2,21 +2,23 @@ import React from 'react';
 import './App.css';
 import AddEntry from '../AddEntry.jsx';
 import CurrentEntries from '../CurrentEntries.jsx';
-import Footer from '../Footer.jsx'
+import MyGitHub from '../MyGitHub.jsx';
+import Footer from '../Footer-Broyles.jsx'
 
 function App() {
 
-  return (
-    <div className="App">
-      <h1>Entries</h1>
+    return (
+        <div className="App">
+            <h1>Entries</h1>
 
-      <AddEntry />
-      <hr />
-      <CurrentEntries />
-      <hr />
-      <Footer />
-    </div>
-  )
+            <AddEntry />
+            <hr />
+            <CurrentEntries />
+            <hr />
+            <Footer />
+            <MyGitHub></MyGitHub>
+        </div>
+    )
 }
 
 export default App;
