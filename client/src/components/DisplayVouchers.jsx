9776a8589f68591ae1c632777736be7b09f722ac
jjbrown23 <71121776+jjbrown23@users.noreply.github.com>
2022-12-09 01:05:06 -0700

    att 8

--- client/src/components/DisplayVouchers.jsx
+++ client/src/components/DisplayVouchers.jsx
@@ -89,7 +89,7 @@ const DisplayVouchers = () => {
       getNewVolunteerCount() 
       
       const updateBtn = document.getElementById('updateCountsButton')
-      updateBtn.display = 'inline'
+      updateBtn.display = 'block';
 
     }
   
