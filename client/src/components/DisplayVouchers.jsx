54e01b417a64717eab4b7f3957bf57d558234da9
Waterboy1887 <connorw.cc@gmail.com>
2022-12-09 10:38:01 -0700

    Made a toggle to make the number of vouchers flash red

--- client/src/components/DisplayVouchers.jsx
+++ client/src/components/DisplayVouchers.jsx
@@ -1,28 +1,41 @@
-const VoucherCount = 11;
+import React, { useState, useEffect } from 'react';
+
+const VoucherCount = 5;
 const countLow = {
   color: 'red',
 };
 
-const High = () => {
-  return <b>{VoucherCount}</b>;
-};
-
-const Low = () => {
-  return <b style={countLow}>{VoucherCount}</b>
-};
-
-function ShowCount(VoucherCount) {
-  if (VoucherCount <= 10) {
-    return <Low/>;
-  }
-  return <High/>;
-};
-
 const DisplayVouchers = () => {
+  const [seconds, setSeconds] = useState(10);
+
+  useEffect(() => {
+    const time = seconds > 0 && setInterval(() => setSeconds(seconds - 1), 1000);
+    return () => clearInterval(time);
+  }, [seconds]);
+
+  const High = () => {
+    return <b>{VoucherCount}</b>;
+  };
+  
+  const Low = (seconds) => {
+    if (seconds % 2 === 0) {
+      return <b style={countLow}>{VoucherCount}</b>;
+    }
+    else if (seconds % 2 === 1) {
+      return <b>{VoucherCount}</b>;
+    }
+  };
+  
+  function ShowCount(VoucherCount) {
+    if (VoucherCount <= 10) {
+      return Low(seconds);
+    }
+    return <High/>;
+  };  
 
   return (
-    <div class='editControls'>
-      <h4>Vouchers Remaining: {ShowCount(VoucherCount)}</h4>
+    <div>
+      <h4>Available Vouchers: {ShowCount(VoucherCount)}</h4>
     </div>
   );
 };
