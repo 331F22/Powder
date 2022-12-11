f2fb6a113effe6fb3de2d4a4fda53c9c87e6f1ca
Grant Dunbar <grantdunbar8@gmail.com>
2022-12-11 16:50:42 -0700

    testing

--- client/src/components/UploadVouchers.jsx
+++ client/src/components/UploadVouchers.jsx
@@ -1,19 +1,39 @@
 import React, { useState } from "react";
+import { useEffect } from "react";
 import Modal from "./Modal";
 
 const VoucherManagement = () => {
-
+  const [numCodes, setNumCodes] = useState(0)
   const [openModal, setOpenModal] = useState(false);
   const open = () => {
     setOpenModal(true)
     document.querySelector('body').classList.add('modal-open')
   }
+
+  useEffect(()=>{
+    fetch("https://api.example.com/items")
+      .then(res => res.json())
+      .then(
+        (result) => {
+          console.log(result)
+        }
+      )
+    // setNumCodes()
+  }, [])
+  
   return (
     <div>
       <h2>Voucher Management</h2>
-      <p>Remaining number of codes will go here</p>
+      {numCodes < 20 ? 
+        <div class="alert alert-danger" role="alert">
+          You are close to running out of codes! Upload more before you run out!
+        </div>
+        : ''
+      }
+      <h4>Remaining Codes:</h4>
+      <h5>{numCodes}</h5>
       <Modal setOpenModal={setOpenModal} openModal={openModal}/>
-      <button className="btn btn-primary" onClick={() => open()}>Add Codes</button>
+      <button className="btn btn-primary btn-add-codes" onClick={() => open()}>Add Codes</button>
     </div>
   );
 }
