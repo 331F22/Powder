1a9d58573403bb9a4f18be66f11913c0d77dbf98
ezra.skoog <q55r879@csci331.cs.montana.edu>
2022-12-12 15:57:38 -0700

    Last commit of semester...hopefully

--- client/src/components/App/App.js
+++ client/src/components/App/App.js
@@ -1,14 +1,29 @@
-import React from 'react';
 import './App.css';
 import AddEntry from '../AddEntry.jsx';
 import CurrentEntries from '../CurrentEntries.jsx';
 import Footer from '../Footer.jsx'
 import Navbar from '../../NavBar/PageNavBar';
 import Layout from '../Layout';
-
+import React, { useState, useRef } from "react";
+import Popup from "reactjs-popup";
+import SignaturePad from "react-signature-canvas";
+import "./sigCanvas.css";
 
 
 function App() {
+
+  const [imageURL, setImageURL] = useState(null); // create a state that will contain our image url
+  const sigCanvas = useRef({});
+
+  /* a function that uses the canvas ref to clear the canvas
+  via a method given by react-signature-canvas */
+  const clear = () => sigCanvas.current.clear();
+
+  /* a function that uses the canvas ref to trim the canvas
+  from white spaces via a method given by react-signature-canvas
+  then saves it in our state */
+  const save = () =>
+    setImageURL(sigCanvas.current.getTrimmedCanvas().toDataURL("image/png"));
 
   return (
     <div>
@@ -16,9 +31,46 @@ function App() {
                 <img id='header_img' src='https://uploads-ssl.webflow.com/57b4d56c1f986d4879b0574d/581d0395c6f121fb068e4d22_BSFlogo-p-1080x664.jpeg' />
                 <h1>volunteer page</h1>
             </div>
-      {/* <h1 id='home_page_header'>Entries</h1> */}
       <div className="App">
         <AddEntry />
+        <hr />
+      <h1>Sign here</h1>
+      <Popup
+        modal
+        trigger={<button>Open Signature Pad</button>}
+        closeOnDocumentClick={false}
+      >
+        {close => (
+          <>
+            <SignaturePad
+              ref={sigCanvas}
+              canvasProps={{
+                className: "signatureCanvas"
+              }}
+            />
+            {/* Button to trigger save canvas image */}
+            <button onClick={save}>Save</button>
+            <button onClick={clear}>Clear</button>
+            <button onClick={close}>Close</button>
+          </>
+        )}
+      </Popup>
+      <br />
+      <br />
+      {/* if our we have a non-null image url we should
+      show an image and pass our imageURL state to it*/}
+      {imageURL ? (
+        <img
+          src={imageURL}
+          alt="my signature"
+          style={{
+            display: "block",
+            margin: "0 auto",
+            border: "1px solid black",
+            width: "150px"
+          }}
+        />
+      ) : null}
         <hr />
         <Footer />
       </div>
