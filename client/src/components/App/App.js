f83abbaa876aef09a9de82a7ec0d4876adcdb6de
Ezra1285 <ezraskoog@yahoo.com>
2022-12-11 17:00:50 -0700

    finished the header for the home page.

--- client/src/components/App/App.js
+++ client/src/components/App/App.js
@@ -11,14 +11,17 @@ import Layout from '../Layout';
 function App() {
 
   return (
-    <div className="App">
-      <h1>Entries</h1>
-
-      <AddEntry />
-      <hr />
-      {/* <CurrentEntries /> */}
-      <hr />
-      <Footer />
+    <div>
+        <div id='top_header'>
+                <img id='header_img' src='https://uploads-ssl.webflow.com/57b4d56c1f986d4879b0574d/581d0395c6f121fb068e4d22_BSFlogo-p-1080x664.jpeg' />
+                <h1>volunteer page</h1>
+            </div>
+      {/* <h1 id='home_page_header'>Entries</h1> */}
+      <div className="App">
+        <AddEntry />
+        <hr />
+        <Footer />
+      </div>
     </div>
   )
 }
