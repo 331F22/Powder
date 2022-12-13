cfca26960b8ee9093e7a70591f45b779c38b8357
mkultradotgov <48577305+mkultradotgov@users.noreply.github.com>
2022-12-12 18:55:25 -0700

    removing default stuff

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import './App.css';
-import AddEntry from './AddEntry.jsx';
-import CurrentEntries from './CurrentEntries.jsx';
 //import Footer from '../Footer.jsx'
 import Dropdown from './Dropdown';
 import Header from './Header.js';
@@ -15,11 +13,6 @@ function App() {
       <div>
         <Header/>
       </div>
-      <h1 id='first'>Welcome Powder enthusiasts!</h1>
-      <div className='entry'>
-      <AddEntry />
-      </div>
-      <CurrentEntries />
       <hr />
       <h2 className='trail'>Trail Maps</h2>
 	    <div className='drop'>
